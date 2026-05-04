ff82aa94c6ac5dfdcfafe84c22a47d0024254c04
Yusuf Afif <yusufmafif@gmail.com>
2026-05-04 20:38:07 +0700

    d

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
 
