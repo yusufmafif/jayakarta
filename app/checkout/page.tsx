15320bc2b4aae024e50eb2620c01c29c827d3c23
Yusuf Afif <yusufmafif@gmail.com>
2026-05-06 21:55:06 +0700

    www

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,5 @@
 import CheckoutPage from "./CheckoutPage";
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
-  return <CheckoutPage planId={searchParams?.plan as string} />;
+export default function Page() {
+  return <CheckoutPage />;
 }
