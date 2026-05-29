3fd56400d0de3ea3123a588949f092d5c5f7556b
Yusuf Afif <yusufmafif@gmail.com>
2026-05-29 13:51:35 +0700

    env dynamic

--- app/api/tokenizer/route.js
+++ app/api/tokenizer/route.js
@@ -2,7 +2,7 @@ import Midtrans from "midtrans-client"
 import { NextResponse } from 'next/server';
 
 let snap = new Midtrans.Snap({
-  isProduction: false,
+  isProduction: process.env.ENVIRONMENT,
   serverKey: process.env.SECRET,
   clientKey: process.env.NEXT_PUBLIC_CLIENT,
 })
