78cf33e6973f165cf6837943b414950c46f01cc8
Yusuf Afif <yusufmafif@gmail.com>
2026-05-29 14:03:29 +0700

    test

--- app/api/tokenizer/route.js
+++ app/api/tokenizer/route.js
@@ -2,46 +2,36 @@ import Midtrans from "midtrans-client"
 import { NextResponse } from 'next/server';
 
 let snap = new Midtrans.Snap({
-  isProduction: process.env.ENVIRONMENT === "production",
+  isProduction: true,
   serverKey: process.env.SECRET,
   clientKey: process.env.NEXT_PUBLIC_CLIENT,
 })
 
 export async function POST(request) {
-  try {
-    const { id, productName, price, quantity } = await request.json()
+  const { id, productName, price, quantity } = await request.json()
 
-    const origin =
-      request.headers.get('origin') || 'http://localhost:3000';
+  // 1. Ambil URL Dev Tunnels yang sedang aktif secara otomatis dari browser
+  const origin = request.headers.get('origin') || 'http://localhost:3000';
 
-    let parameter = {
-      item_details: [
-        {
-          name: productName,
-          price: price,
-          quantity: quantity
-        }
-      ],
-      transaction_details: {
+  let parameter = {
+    item_details: {
+        name: productName,
+        price: price,
+        quantity: quantity
+    },
+    transaction_details: {
         order_id: id,
         gross_amount: price * quantity
-      },
-      callbacks: {
-        finish: `${origin}/thanks`,
-        error: `${origin}/`,
-        pending: `${origin}/`
-      }
+    },
+    // 2. Tambahkan callbacks di bawah ini agar menimpa pengaturan dashboard
+    callbacks: {
+      finish: `${origin}/thanks`,      // Halaman tujuan jika sukses (misal kembali ke beranda)
+      error: `${origin}/`,       // Halaman jika gagal
+      pending: `${origin}/`      // Halaman jika pending
     }
+  } 
 
-    const token = await snap.createTransactionToken(parameter)
-
-    return NextResponse.json({ token })
-
-  } catch (err) {
-    console.error(err)
-    return NextResponse.json(
-      { error: err.message },
-      { status: 500 }
-    )
-  }
+  const token = await snap.createTransactionToken(parameter)
+  console.log(token)
+  return NextResponse.json({ token })
 }
